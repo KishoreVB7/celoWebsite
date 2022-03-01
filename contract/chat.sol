3280615c572dece423d7952d774b7b7f7f857021
Kishore <kishore07kzr@gmail.com>
2022-03-01 09:48:10 +0530

    added modifiers, events, rejoin functionality

--- contract/chat.sol
+++ contract/chat.sol
@@ -2,8 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "hardhat/console.sol";
-
 interface IERC20Token {
     function transfer(address, uint256) external returns (bool);
 
@@ -185,8 +183,9 @@ library SafeMath {
 }
 
 contract Chat {
+    //The address of the user who is waiting to be matched
     address internal waitingAddress;
-    address internal cUsdTokenAddress =
+    address internal constant cUsdTokenAddress =
         0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
     using SafeMath for uint256;
 
@@ -196,20 +195,46 @@ contract Chat {
         uint256 timestamp;
     }
 
+    //The mapping used for pairing one user with another
     mapping(address => address) internal addresses;
+
+    //Mapping used to store messages
     mapping(address => mapping(uint256 => Message)) internal messages;
+
+    //Mapping used to store the value one sent another
     mapping(address => uint256) internal messageAmounts;
 
-    // assigns an address to an address if there's one waiting,
-    // if not, it makes the sender address the address that is waiting
+    //Mapping used to store the user last match with
+    mapping( address => address) internal previousMatch;
+
+    //Modifier that is used to check if the caller has a partner
+    modifier hasPartner(){
+        require(isAddressAssigned(),"You are not matched with an address");
+        _;
+    }
+
+    //Event that is emmited when user is connected
+    event connected(address indexed Person1, address indexed person2);
+
+    //Event that is emmited when there is a fund transfer between the users
+    event transferBetweenUsers(address indexed sender, address indexed reciever, uint value);
+
+    /* checks if there's one waiting,
+    if not, it makes the sender address the address that is waiting */
     function assignAddress() public {
+        /* checks if the waiting address is empty (address(0) means that if the address is empty) */
         if (waitingAddress == address(0)) {
             waitingAddress = msg.sender;
+        /* If the above statement is not true, it means that someone is already waiting, 
+            The below statement checks that if the caller is waiting, if not then he is matched with 
+            the waiting address
+        */
         } else if (waitingAddress != msg.sender) {
             addresses[msg.sender] = waitingAddress;
             addresses[waitingAddress] = msg.sender;
             waitingAddress = address(0);
         }
+        emit connected(msg.sender, waitingAddress);
     }
 
     // says if address is waiting
@@ -228,30 +253,35 @@ contract Chat {
     }
 
     // writes a message to someone
-    function writeMessage(string memory _text) public {
-        if (isAddressAssigned()) {
-            address destination = addresses[msg.sender];
-            messages[destination][messageAmounts[destination]++] = Message(
-                msg.sender,
-                _text,
-                block.timestamp
-            );
-        }
+    function writeMessage(string memory _text) public hasPartner{
+        address destination = addresses[msg.sender];
+        messages[destination][messageAmounts[destination]++] = Message(
+            msg.sender,
+            _text,
+            block.timestamp
+        );
+    }
+
+    //Connect again with the last disconnected person
+    function connectWithPreviousMatch() public{
+        require(previousMatch[msg.sender] == waitingAddress);
+        addresses[msg.sender] = waitingAddress;
+        addresses[waitingAddress] = msg.sender;
+        waitingAddress = address(0);
     }
 
     // transfers funds and sends a confirmation message to someone
-    function transferFunds(uint256 _amount) public payable {
-        if (isAddressAssigned()) {
-            require(
-                IERC20Token(cUsdTokenAddress).transferFrom(
-                    msg.sender,
-                    addresses[msg.sender],
-                    _amount
-                ),
-                "Transfer failed."
-            );
-            writeMessage(string(abi.encodePacked("cUSD--", uint2str(_amount))));
-        }
+    function transferFunds(uint256 _amount) public payable hasPartner {
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                addresses[msg.sender],
+                _amount
+            ),
+            "Transfer failed."
+        );
+        writeMessage(string(abi.encodePacked("cUSD--", uint2str(_amount))));
+        emit transferBetweenUsers(msg.sender, addresses[msg.sender], _amount);
     }
 
     // gets the length of the received message mapping of an adress
@@ -290,24 +320,25 @@ contract Chat {
 
     // removes a match
     function removeMatch() public {
-        if (addresses[msg.sender] != address(0)) {
-            // delete recived messages
-            for (uint256 i = 0; i == messageAmounts[msg.sender]; i++) {
-                delete messages[msg.sender][i];
-            }
-            // delete sent messages
-            for (
-                uint256 i = 0;
-                i == messageAmounts[addresses[msg.sender]];
-                i++
-            ) {
-                delete messages[addresses[msg.sender]][i];
-            }
-            delete messageAmounts[addresses[msg.sender]];
-            delete messageAmounts[msg.sender];
-            delete addresses[addresses[msg.sender]];
-            delete addresses[msg.sender];
+        require(addresses[msg.sender] != address(0), "You don't have a match");
+        previousMatch[msg.sender] = addresses[msg.sender];
+        previousMatch[addresses[msg.sender]] = msg.sender;
+        // delete received messages
+        for (uint256 i = 0; i == messageAmounts[msg.sender]; i++) {
+            delete messages[msg.sender][i];
         }
+        // delete sent messages
+        for (
+            uint256 i = 0;
+            i == messageAmounts[addresses[msg.sender]];
+            i++
+        ) {
+            delete messages[addresses[msg.sender]][i];
+        }
+        delete messageAmounts[addresses[msg.sender]];
+        delete messageAmounts[msg.sender];
+        delete addresses[addresses[msg.sender]];
+        delete addresses[msg.sender];
     }
 
     // converts a number into a string
